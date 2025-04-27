52e79a5a5cc8bdda914eb6eaa9f68cfa5038e0cb
S-Leishem <soubamleishem4@gmail.com>
2025-04-27 22:38:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function showEmail(event) {
+    event.preventDefault();
+    const emailDisplay = document.getElementById('email-display');
+    emailDisplay.textContent = "Mail me on: soubamleishem4@gmail.com";
+}
